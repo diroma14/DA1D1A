78fa0dea5c6d280be6889b27efa2a7df15528bbf
diroma14 <114869989+diroma14@users.noreply.github.com>
2025-04-23 12:52:26 +0200

    Delete BaseDatos directory

--- BaseDatos/triggers/trigger.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE TRIGGER trigger_name
-[AFTER \ BEFORE] [DELETE\INSERT\UPDATE]
-ON table_name FOR EACH ROW
-    INSERT INTO logs(log)
-    VALUES(CONCAT("Se ha eliminado old.name"));
-
-
